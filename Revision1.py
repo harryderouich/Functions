0132e2b8b6dcbe0d63cda50bc0035b9c3069d9bd
harryderouich <40683@student.longroad.ac.uk>
2014-11-21 14:30:33 +0000

    Starter done and revision exercises started

---
+++ Revision1.py
@@ -0,0 +1,20 @@
+#Harry Derouich
+#21/11/14
+#Functions class exercises - Revision 1
+
+def input_values():
+    number = int(input("Please enter a number :"))
+    character = input("Please enter a character :")
+    return number, character
+
+def print_symbols(total):
+    print(total)
+
+def output_symbols(number, character):
+    total = character * number
+    return total
+    
+
+input_values()
+print_symbols(total)
+output_symbols(number, character)
--- starter exercises.py
+++ starter exercises.py
@@ -3,39 +3,36 @@
 #Functions starter exercise
 
 def calculate_basic_pay(hours, rate):
-    total = hours * pay
+    total = hours * rate
     return total
 
 def calculate_overtime_pay(hours, rate):
     overtime_rate = rate * 1.5
     overtime_hours = hours - 40
-    total = overtime_hours * overtime_rate
-    return total
-
-def calculate_total_pay(hours, rate):
-    if hours <= 40:
-        total = calculate_basic_pay
-    else:
-        total = calculate_overtime_pay
+    total = overtime_hours * overtime_rate + (40 * rate)
     return total
 
 def work_details():
     hours = int(input("Please enter the total hours worked: "))
     rate = int(input("Please enter your basic rate of pay: "))
-    return hours
-    return rate
+    return hours, rate
 
-def display_total_pay(calculate_total_pay):
-    output = print(calculate_total_pay)
-    return output
+def calculate_total_pay(hours, rate):
+    if hours <= 40:
+        total = calculate_basic_pay(hours, rate)
+    else:
+        total = calculate_overtime_pay(hours, rate)
+    return total
+
+def display_total_pay(total):
+    print(total)
 
 def calculate_pay():
     hours, rate = work_details()
-    total_pay = calculate_total_pay(hours, rate)
-    print(total_pay)
+    total = calculate_total_pay(hours, rate)
+    display_total_pay(total)
 
-work_details()
-total = calculate_total_pay(hours, rate)
-print(total)
+calculate_pay()
+
 
 
